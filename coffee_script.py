aefe701cefaebef538d68e3997788f2a4007c7c4
Gabrijel Škoro <gabrijel.skoro@gmail.com>
2016-11-28 22:24:09 +0100

    Whitespace fix

--- coffee_script.py
+++ coffee_script.py
@@ -10,5 +10,5 @@ while True:
   input_state = GPIO.input(18)
   if input_state == False:
     headers = {'Auth': '123'}
-	requests.post('http://gabrijelskoro.com/api/click', headers = headers)
+    requests.post('http://gabrijelskoro.com/api/click', headers = headers)
     time.sleep(0.2)
